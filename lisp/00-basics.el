d4511297106c99c80ebc21d0a245b6e4eeab08f2
Guangwang Huang <whatacold@gmail.com>
2022-03-04 23:50:04 +0800

    use the built-in auto-save-visited-mode instead.

--- lisp/00-basics.el
+++ lisp/00-basics.el
@@ -1,24 +1,29 @@
 ;;; auto save files
-(defvar w/auto-save--timer nil)
+;; (defvar w/auto-save--timer nil)
+;;
+;; (defun w/auto-save-enable ()
+;;     "Save all unsaved filed when being idle."
+;;   (interactive)
+;;   (w/auto-save-disable)
+;;   (setq w/auto-save--timer
+;; 	(run-with-idle-timer
+;; 	 3
+;; 	 'repeat
+;; 	 (lambda ()
+;; 	   (save-some-buffers t)))))
+;;
+;; (defun w/auto-save-disable ()
+;;     "Disable auto-saving files."
+;;   (interactive)
+;;   (when w/auto-save--timer
+;;     (cancel-timer w/auto-save--timer)))
+;;
+;; (w/auto-save-enable)
 
-(defun w/auto-save-enable ()
-    "Save all unsaved filed when being idle."
-  (interactive)
-  (w/auto-save-disable)
-  (setq w/auto-save--timer
-	(run-with-idle-timer
-	 3
-	 'repeat
-	 (lambda ()
-	   (save-some-buffers t)))))
+;; built-in auto save files
+(setq auto-save-visited-interval 2)
+(auto-save-visited-mode)
 
-(defun w/auto-save-disable ()
-    "Disable auto-saving files."
-  (interactive)
-  (when w/auto-save--timer
-    (cancel-timer w/auto-save--timer)))
-
-(w/auto-save-enable)
 
 ;;; dired
 (setq dired-dwim-target t)
