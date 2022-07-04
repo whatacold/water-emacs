863858908d549d8300f92a96da7aaa04bcf23289
Guangwang Huang <whatacold@gmail.com>
2022-07-04 22:18:19 +0800

    fine-tune hippie-expand.

--- lisp/10-writing.el
+++ lisp/10-writing.el
@@ -19,6 +19,18 @@
            company-idle-delay 0.2
            company-require-match nil)))
 
+;;; hippie-expand
+(setq hippie-expand-try-functions-list
+  '(try-complete-file-name-partially
+    try-complete-file-name
+    try-expand-all-abbrevs
+    try-expand-list
+    ;; try-expand-line
+    try-expand-dabbrev
+    try-expand-dabbrev-all-buffers
+    try-expand-dabbrev-from-kill
+    try-complete-lisp-symbol-partially
+    try-complete-lisp-symbol))
 
 ;;; org-mode
 (setq org-adapt-indentation nil) ; don't indent to outline node level
