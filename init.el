63f40adcba5b8d2156d685aa916e6e3cee9deb09
Guangwang Huang <whatacold@gmail.com>
2022-09-12 15:52:03 +0800

    better font settings.

--- init.el
+++ init.el
@@ -9,8 +9,56 @@
 (defcustom w/theme 'tsdh-light
   "The UI theme.")
 
-(defcustom w/font-and-size "DejaVu Sans Mono-16"
-  "The font and its size.")
+(defcustom w/font-size "16"
+  "The font size.")
+
+(defcustom w/default-font (cond
+                           ((string-equal system-type "windows-nt") ; Microsoft Windows
+                            (when (member "Consolas" (font-family-list))
+                              "Consolas"))
+                           ((string-equal system-type "darwin") ; macOS
+                            (when (member "Menlo" (font-family-list))
+                              "Menlo"))
+                           ((string-equal system-type "gnu/linux") ; linux
+                            (when (member "DejaVu Sans Mono" (font-family-list))
+                              "DejaVu Sans Mono")))
+  "The default font.")
+
+(defcustom w/chinese-font (cond
+                           ((string-equal system-type "windows-nt")
+                            (cond
+                             ((member "Microsoft YaHei" (font-family-list)) "Microsoft YaHei")
+                             ((member "Microsoft JhengHei" (font-family-list)) "Microsoft JhengHei")
+                             ((member "SimHei" (font-family-list)) "SimHei")))
+                           ((string-equal system-type "darwin")
+                            (cond
+                             ((member "Hei" (font-family-list)) "Hei")
+                             ((member "Heiti SC" (font-family-list)) "Heiti SC")
+                             ((member "Heiti TC" (font-family-list)) "Heiti TC")))
+                           ((string-equal system-type "gnu/linux")
+                            (cond
+                             ((member "WenQuanYi Micro Hei" (font-family-list)) "WenQuanYi Micro Hei"))))
+  "Chinese font.")
+
+(defcustom w/symbol-font (cond
+                          ((string-equal system-type "windows-nt")
+                           (cond
+                            ((member "Segoe UI Symbol" (font-family-list)) "Segoe UI Symbol")))
+                          ((string-equal system-type "darwin")
+                           (cond
+                            ((member "Apple Symbols" (font-family-list)) "Apple Symbols")))
+                          ((string-equal system-type "gnu/linux")
+                           (cond
+                            ((member "Symbola" (font-family-list)) "Symbola"))))
+  "Symbol font, excluding emoji.")
+
+(defcustom w/emoji-font (cond
+                         ((member "Apple Color Emoji" (font-family-list)) "Apple Color Emoji")
+                         ((member "Noto Color Emoji" (font-family-list)) "Noto Color Emoji")
+                         ((member "Noto Emoji" (font-family-list)) "Noto Emoji")
+                         ((member "Segoe UI Emoji" (font-family-list)) "Segoe UI Emoji")
+                         ((member "Symbola" (font-family-list)) "Symbola"))
+  "Emoji font.")
 
 (defcustom w/python-venv nil
   "The Python virtual environment.")
@@ -51,8 +99,18 @@
 ;;; UI
 (load-theme w/theme 'no-confirm)
 ;; font & font size
-(add-to-list 'default-frame-alist
-             `(font . ,w/font-and-size))
+;; http://xahlee.info/emacs/emacs/emacs_list_and_set_font.html
+(set-frame-font (format "%s-%s" w/default-font w/font-size) t t)
+(set-fontset-font t 'symbol w/symbol-font)
+(set-fontset-font t
+                  ;; set font for emoji
+                  ;; (if before emacs 28, should come after setting symbols.
+                  ;; emacs 28 now has 'emoji . before, emoji is part of 'symbol)
+                  (if (version< emacs-version "28.1")
+                      '(#x1f300 . #x1fad0)
+                    'emoji)
+                  w/emoji-font)
+(set-fontset-font t 'han w/chinese-font)
 
 (tool-bar-mode -1)
 (menu-bar-mode -1)
