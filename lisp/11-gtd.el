20e3a1ac4ddcd094961ca9d8ad335b45b0f2b186
Guangwang Huang <whatacold@gmail.com>
2023-04-08 15:42:44 +0800

    gtd

---
+++ lisp/11-gtd.el
@@ -0,0 +1,14 @@
+(setq org-todo-keywords
+      '((sequence "TODO" "IN-PROGRESS" "WAITING" "DONE")))
+
+;; agenda
+;; (setq org-agenda-files '("/a" "/b"))  ; set the agenda files
+(eval-after-load 'org-agenda
+  '(progn
+     (add-to-list 'org-agenda-prefix-format '(agenda . "%b%?-12t% s"))))
+(setq org-agenda-breadcrumbs-separator " > ")
+
+;; org-mobile for MobileOrg
+(setq org-mobile-files '("~/org/mobileorg.org" ; the capture file whose name cannot be customized
+                         org-agenda-files) ; files to be staged, should be in absolute path
+      org-mobile-directory "~/mobileorg/") ; the staging area
