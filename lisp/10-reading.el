7ddbf811cd87d8f6ea657dfac855649710a31295
Ken Huang <whatacold@gmail.com>
2024-03-16 10:52:32 +0800

    use pdf-tools

---
+++ lisp/10-reading.el
@@ -0,0 +1,9 @@
+(use-package pdf-tools
+  :ensure t
+  :pin manual ; don't reinstall when package updates
+  :mode  ("\\.pdf\\'" . pdf-view-mode)
+  :config
+  (setq-default pdf-view-display-size 'fit-page)
+  ;; (setq pdf-annot-activate-created-annotations t)
+  (pdf-tools-install 'no-query)
+  (require 'pdf-occur))
