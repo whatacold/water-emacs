b7c8fb8da28e2c43861c4833019f08d248bb7e80
Ken Huang <whatacold@gmail.com>
2024-04-04 23:15:23 +0800

    Add tempel

--- lisp/10-programming.el
+++ lisp/10-programming.el
@@ -151,6 +151,41 @@ If `specify-project-p' is non-nil, prompt users to select a project."
   :bind (("C-c s e" . aya-expand)
          ("C-c s c" . aya-create)))
 
+(use-package tempel
+  ;; Require trigger prefix before template name when completing.
+  ;; :custom
+  ;; (tempel-trigger-prefix "<")
+  :ensure t
+  :bind (; ("M-+" . tempel-complete) ;; Alternative tempel-expand
+         ("C-c s i" . tempel-insert))
+
+  :init
+  (setq tempel-path '("~/.emacs.d/data/tempel-templates/*.eld"
+                      "~/.emacs.local.d/tempel-templates/*.eld"))
+
+  ;; Setup completion at point
+  (defun tempel-setup-capf ()
+    ;; Add the Tempel Capf to `completion-at-point-functions'.
+    ;; `tempel-expand' only triggers on exact matches. Alternatively use
+    ;; `tempel-complete' if you want to see all matches, but then you
+    ;; should also configure `tempel-trigger-prefix', such that Tempel
+    ;; does not trigger too often when you don't expect it. NOTE: We add
+    ;; `tempel-expand' *before* the main programming mode Capf, such
+    ;; that it will be tried first.
+    (setq-local completion-at-point-functions
+                (cons #'tempel-expand
+                      completion-at-point-functions)))
+
+  (add-hook 'prog-mode-hook 'tempel-setup-capf)
+  (add-hook 'conf-mode-hook 'tempel-setup-capf)
+  (add-hook 'text-mode-hook 'tempel-setup-capf)
+
+  ;; Optionally make the Tempel templates available to Abbrev,
+  ;; either locally or globally. `expand-abbrev' is bound to C-x '.
+  ;; (add-hook 'prog-mode-hook #'tempel-abbrev-mode)
+  ;; (global-tempel-abbrev-mode)
+)
+
 (defun w/urxvt ()
   "Open a new urxvt terminal based on `default-directory' of the current buffer."
   (interactive)
